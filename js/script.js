ac600b28fdc95bdaa31b367d31d2374e36413e48
Sergiod3x <sergiod3x.@gmail.com>
2021-06-28 18:22:34 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,19 @@
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+           photos: [
+            "./img/1.jpg",
+            "./img/2.jpg",
+            "./img/3.jpg",
+            "./img/4.jpg",
+            
+           ]
+        }
+    }
+       
+ );
+  
+
